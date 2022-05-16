cc04ed2b1ba4e940c78bc7cd6e290057b41ded19
7117 <qinglangsalaheiyou@163.com>
2022-05-16 20:08:05 +0800

    long

--- practice/interview-exam/004最长公共前缀.go
+++ practice/interview-exam/004最长公共前缀.go
@@ -5,24 +5,15 @@ import (
 	"strings"
 )
 
-//编写一个函数来查找字符串数组中的最长公共前缀。
-func longestCommonPrefix(strs []string) string {
-	if len(strs) == 0 {
-		return ""
-	}
-	firstStr := strs[0]
-	for i := 1; i < len(strs); i++ {
-		for strings.Index(strs[i], firstStr) != 0 {
-			firstStr = firstStr[:len(firstStr)-1]
+func main() {
+	s := []string{"flower", "flow", "flight"}
+	a := s[0]
+
+	for k, _ := range s {
+		for strings.Index(s[k], a) != 0 {
+			a = a[:len(a)-1]
 		}
 	}
-	if len(firstStr) > 0 {
-		return firstStr
-	}
-	return ""
-}
 
-func main() {
-	s := []string{"flower", "flow", "floght"}
-	fmt.Println(longestCommonPrefix(s))
+	fmt.Println(a)
 }
